5f83dc9b68fbd201873a39ffa554e83f71543d73
carlosuc3m <100329787@alumnos.uc3m.es>
2024-02-26 16:14:20 +0100

    ready to deploy

---
+++ src/main/java/plugins/tinevez/imglib2icy/ImgLib2RoiIcy.java
@@ -0,0 +1,13 @@
+package plugins.tinevez.imglib2icy;
+
+import icy.plugin.abstract_.Plugin;
+import icy.plugin.interface_.PluginLibrary;
+
+/**
+ * A JAR file to be able to use ImgLib2-roi inside of Icy
+ * 
+ * @author Carlos Garcia Lopez de Haro
+ */
+public class ImgLib2RoiIcy extends Plugin implements PluginLibrary
+{
+}
